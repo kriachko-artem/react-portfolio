662f1551e2341262d7af1320e387c8e218c5612e
artemkriachko <kriachkoartem@gmail.com>
2022-06-20 16:11:34 +0300

    creating

--- src/components/3D Cards/Cards.js
+++ src/components/3D Cards/Cards.js
@@ -3,9 +3,12 @@ import './cards.css'
 import firstCardImg from './images/first.png'
 import secondCardImg from './images/second.png'
 import thirdCardImg from './images/third.png'
+
 import {deviceOrientation3D, setBigCursor} from "../../animations/animations";
 import {mouseMoveCard3D} from "../../animations/animations";
 
+const devices = new RegExp('Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini', "i");
+const isMobile = devices.test(navigator.userAgent);
 function getAccess(){
     DeviceOrientationEvent.requestPermission()
         .then(() =>{
@@ -18,10 +21,12 @@ function getAccess(){
 
 export function Cards () {
     useEffect(()=>{
+        if (isMobile === false){
+            setBigCursor('.card_img-holder img')
+            setBigCursor('.get-access')
+            mouseMoveCard3D('.card_img-holder')
+        }
 
-
-        setBigCursor('.card_img-holder img')
-        mouseMoveCard3D('.card_img-holder')
     },[])
 
   return (
@@ -29,11 +34,21 @@ export function Cards () {
           <div className="container">
             <hr/>
               <div className="cards_content">
-                  <h2 className="cards_content__tittle">3D cards</h2>
-                  <button onClick={getAccess} className={'get-access'}>Start 3D</button>
-                  <div className="cards_content__main">
-                     <h4>You can move mouse on this cards to see the my custom 3D effect to card</h4>
-                      <div className="cards_content__main__cards">
+                  <div className="cards_content__tittle">
+                      <h2>3D cards</h2>
+                      <div className="cards_content__description">
+                          {isMobile? (
+                              <>
+                                  <h4 className={'description-rotate'}>You can rotate your phone to see the my custom 3D effect to card</h4>
+                                  <button onClick={getAccess} className={'get-access'}>Start 3D</button>
+                              </>
+                          ):(
+                              <h4>You can move mouse on this cards to see the my custom 3D effect to card</h4>
+                          )}
+                      </div>
+                  </div>
+                  <div className="cards_content__main__cards">
+                      {isMobile? (
                           <div className="card">
                               <div className="card_img-holder">
                                   <div className="card-3d">
@@ -46,10 +61,59 @@ export function Cards () {
                                   </div>
                               </div>
                               <h5 className="card_name">
-                                 Card name
+                                  Card name
                               </h5>
                           </div>
-                      </div>
+                      ):(
+                          <>
+                              <div className="card">
+                                  <div className="card_img-holder">
+                                      <div className="card-3d">
+                                          <img src={firstCardImg} alt="Plan your trip"/>
+                                          <span className={'left'}/>
+                                          <span className={'right'}/>
+                                          <span className={'top'}/>
+                                          <span className={'bottom'}/>
+                                          <span className={'back'}/>
+                                      </div>
+                                  </div>
+                                  <h5 className="card_name">
+                                      Card one
+                                  </h5>
+                              </div>
+                              <div className="card">
+                                  <div className="card_img-holder">
+                                      <div className="card-3d">
+                                          <img src={secondCardImg} alt="Plan your trip"/>
+                                          <span className={'left'}/>
+                                          <span className={'right'}/>
+                                          <span className={'top'}/>
+                                          <span className={'bottom'}/>
+                                          <span className={'back'}/>
+                                      </div>
+                                  </div>
+                                  <h5 className="card_name">
+                                      Card two
+                                  </h5>
+                              </div>
+                              <div className="card">
+                                  <div className="card_img-holder">
+                                      <div className="card-3d">
+                                          <img src={thirdCardImg} alt="Plan your trip"/>
+                                          <span className={'left'}/>
+                                          <span className={'right'}/>
+                                          <span className={'top'}/>
+                                          <span className={'bottom'}/>
+                                          <span className={'back'}/>
+                                      </div>
+                                  </div>
+                                  <h5 className="card_name">
+                                      Card three
+                                  </h5>
+                              </div>
+                          </>
+                      )}
+
                   </div>
               </div>
           </div>
