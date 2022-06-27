0a960a93481cb85c6bbce6828ea3ad285525a453
artemkriachko <kriachkoartem@gmail.com>
2022-06-27 17:14:07 +0300

    creating

--- src/components/WelcomePage/Welcome.js
+++ src/components/WelcomePage/Welcome.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React from "react";
 import './Welcome.css'
 import {WelcomeSVG} from "./images/WelcomeSVG";
 import {Cursor} from "../Cursor/Cursor";
