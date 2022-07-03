ef89b845bda3e40468d368977e49b44fc06963a2
kriachko-artem <kriachkoartem@gmail.com>
2022-07-03 21:57:32 +0300

    Update BaseLayout.js
    Add Welcome

--- src/components/BaseLayout/BaseLayout.js
+++ src/components/BaseLayout/BaseLayout.js
@@ -10,5 +10,5 @@ export function BaseLayout () {
 
     const [isWelcome,setIsWelcome] = useState(true);
 
-  return !isWelcome? <Welcome setIsWelcome={setIsWelcome}/>: <Sections/>
+  return isWelcome? <Welcome setIsWelcome={setIsWelcome}/>: <Sections/>
 }
